40cb0afe7e6fac11758e0ac3af1f8e0f76222caa
Chad Donohue <chad.donohue@gmail.com>
2016-08-11 19:15:17 -0500

    move some things to src and implement app packaging

---
+++ src/main.js
@@ -0,0 +1,50 @@
+const { app, Menu, Tray } = require("electron");
+const { exec, spawn } = require("child_process");
+
+let trayIcon,
+    caffeinateProcess;
+
+const spawnCaffeinateProcess = () => {
+  caffeinateProcess = spawn("caffeinate")
+};
+
+const killExistingCaffeinateProcess = () => {
+  if (caffeinateProcess) {
+    caffeinateProcess.kill();
+  }  
+}
+
+const getCurrentIcon = (isCaffeinating) => {
+  return `${__dirname}/images/Cup${isCaffeinating ? "Active" : ""}Template.png`;
+}
+
+const appReady = () => {
+  app.dock.hide();
+  let isCaffeinating = false;
+
+  trayIcon = new Tray(`${__dirname}/images/CupTemplate.png`);
+
+  const contextMenu = Menu.buildFromTemplate([{
+    label: "Quit",
+    click: () => {
+      killExistingCaffeinateProcess();
+      trayIcon.destroy();
+      app.quit();
+    },
+  }]);
+
+  trayIcon.on("click", () => {
+    isCaffeinating = !isCaffeinating;
+
+    isCaffeinating ? spawnCaffeinateProcess()
+                   : killExistingCaffeinateProcess();
+
+    trayIcon.setImage(getCurrentIcon(isCaffeinating));
+  });
+
+  trayIcon.on("right-click", () => {
+    trayIcon.popUpContextMenu(contextMenu);
+  });
+}
+
+app.on("ready", appReady);
